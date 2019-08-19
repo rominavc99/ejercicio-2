eb56d0cb5871b36d7e611441f38ab6a48abd4baf
alumno <alumno@E307>
2019-08-19 12:29:37 -0700

    Agregar archivos de proyecto.

---
+++ ejercicio 2/Alumno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    class Alumno
+    {
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+
+        public Alumno()
+        {
+            Nombre = "Pendiente";
+            Matricula = "Sin asignar";
+       
+        }
+
+        public Alumno(string nombre, string matricula)
+        {
+            Nombre = nombre;
+            Matricula = matricula;
+            
+        }
+
+
+
+    }
+
+
+}
---
+++ ejercicio 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+            alumno1.Nombre = "Romina";
+            alumno1.Matricula = "189085";
+
+            Console.WriteLine("Nombre: " + alumno1.Nombre);
+            Console.WriteLine("Matricula: " + alumno1.Matricula);
+
+
+
+            Console.Read();
+        }
+
+        
+    }
+}
